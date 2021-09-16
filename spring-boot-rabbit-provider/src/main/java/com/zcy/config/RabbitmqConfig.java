a17108daf9a0489759968300b780650bfe8b82e7
Inner <826385165@qq.com>
2021-09-16 09:42:47 +0800

    Initial commit

---
+++ spring-boot-rabbit-provider/src/main/java/com/zcy/config/RabbitmqConfig.java
@@ -0,0 +1,13 @@
+package com.zcy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhouchunyang
+ * @Date: Created in 9:41 2021/9/16
+ * @Description:
+ */
+@Configuration
+public class RabbitmqConfig {
+
+}
